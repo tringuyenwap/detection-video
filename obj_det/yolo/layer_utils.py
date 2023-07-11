a28375d4a38df6f8cbd3b12b827e3ad1dc8c49ae
Tringuyen <nguyentri908908@gmail.com>
2023-07-11 22:51:46 +0700

    fix layer

--- obj_det/yolo/layer_utils.py
+++ obj_det/yolo/layer_utils.py
@@ -3,73 +3,47 @@ from __future__ import division, print_function
 
 import numpy as np
 import tensorflow as tf
-import tf_slim as slim
-
-
+from tensorflow.keras import layers
 
 def conv2d(inputs, filters, kernel_size, strides=1):
-    def _fixed_padding(inputs, kernel_size):
-        pad_total = kernel_size - 1
-        pad_beg = pad_total // 2
-        pad_end = pad_total - pad_beg
-
-        padded_inputs = tf.pad(inputs, [[0, 0], [pad_beg, pad_end],
-                                        [pad_beg, pad_end], [0, 0]], mode='CONSTANT')
-        return padded_inputs
-
-    if strides > 1:
-        inputs = _fixed_padding(inputs, kernel_size)
-    inputs = slim.conv2d(inputs, filters, kernel_size, stride=strides,
-                         padding=('SAME' if strides == 1 else 'VALID'))
-    return inputs
-
+    padding = 'same' if strides == 1 else 'valid'
+    return layers.Conv2D(filters, kernel_size, strides=strides, padding=padding)(inputs)
 
 def darknet53_body(inputs):
     def res_block(inputs, filters):
         shortcut = inputs
         net = conv2d(inputs, filters * 1, 1)
         net = conv2d(net, filters * 2, 3)
-
-        net = net + shortcut
-
+        net = layers.Add()([net, shortcut])
         return net
 
-    # first two conv2d layers
     net = conv2d(inputs, 32, 3, strides=1)
     net = conv2d(net, 64, 3, strides=2)
-
-    # res_block * 1
     net = res_block(net, 32)
-
     net = conv2d(net, 128, 3, strides=2)
 
-    # res_block * 2
-    for i in range(2):
+    for _ in range(2):
         net = res_block(net, 64)
 
     net = conv2d(net, 256, 3, strides=2)
 
-    # res_block * 8
-    for i in range(8):
+    for _ in range(8):
         net = res_block(net, 128)
 
     route_1 = net
     net = conv2d(net, 512, 3, strides=2)
 
-    # res_block * 8
-    for i in range(8):
+    for _ in range(8):
         net = res_block(net, 256)
 
     route_2 = net
     net = conv2d(net, 1024, 3, strides=2)
 
-    # res_block * 4
-    for i in range(4):
+    for _ in range(4):
         net = res_block(net, 512)
     route_3 = net
 
     return route_1, route_2, route_3
-
 
 def yolo_block(inputs, filters):
     net = conv2d(inputs, filters * 1, 1)
@@ -81,13 +55,6 @@ def yolo_block(inputs, filters):
     net = conv2d(net, filters * 2, 3)
     return route, net
 
-
 def upsample_layer(inputs, out_shape):
-    # new_height, new_width = out_shape[1], out_shape[2]
-    # # NOTE: here height is the first
-    # # TODO: Do we need to set `align_corners` as True?
-    # inputs = tf.image.resize_nearest_neighbor(inputs, (new_height, new_width), name='upsampled')
-
     new_height, new_width = out_shape[1], out_shape[2]
-    inputs = tf.image.resize(inputs, size=(new_height, new_width), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR, name='upsampled')
-    return inputs
+    return tf.image.resize(inputs, size=(new_height, new_width), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
