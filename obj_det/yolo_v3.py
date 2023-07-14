87c47682b9723670011395175d90b3ba8e54a3ea
Tringuyen <nguyentri908908@gmail.com>
2023-07-14 20:35:50 +0700

    fix yolo

--- obj_det/yolo_v3.py
+++ obj_det/yolo_v3.py
@@ -188,9 +188,12 @@ class YoloV3:
 
             boxes, scores, labels = YoloV3.gpu_nms(pred_boxes_ph, pred_scores_ph, self.num_classes, max_boxes=200, score_thresh=0.1, nms_thresh=0.45)
 
-            # Load the saved model
-            model = tf.keras.models.load_model(graph_path)
+        # Load the saved model checkpoint
+            ckpt = tf.compat.v1.train.Checkpoint(model=yolo_model)
+            ckpt_manager = tf.compat.v1.train.CheckpointManager(ckpt, graph_path, max_to_keep=1)
+            ckpt.restore(ckpt_manager.latest_checkpoint).expect_partial()
         return session
+
 
     @staticmethod
     def gpu_nms(boxes, scores, num_classes, max_boxes=50, score_thresh=0.5, nms_thresh=0.5):
